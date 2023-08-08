121e07a947ab31c7664dec7b51466acd7c2ce185
Sandeepsaini02 <140490677+Sandeepsaini02@users.noreply.github.com>
2023-08-08 23:06:26 +0530

    Add files via upload

---
+++ assingment.m
@@ -0,0 +1,16 @@
+a = 5;
+b = 6;
+c = 3;
+d = 2;
+e = input('a number e = ');
+f= input('a number f = ');
+g = input('a number g = ');
+i = input('a number i = ');
+x = a+e;
+y= a-f;
+z= i/b;
+t = c*g;
+fprintf('%d + %d = %d \n',a,e,x);
+fprintf('%d - %d = %d \n',a,f,y);
+fprintf('%d / %d = %d \n',b,i,z);
+fprintf('%d * %d = %d \n',c,g,t);
